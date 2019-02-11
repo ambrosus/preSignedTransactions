ca767b6be4f1e91293db1e6405dfb4ad97f37b52
Mike-S-Mike <46600126+Mike-S-Mike@users.noreply.github.com>
2019-02-11 13:48:24 +0200

    Added nonce checking to smart contract; Refactoring smart contract

---
+++ readme.md
@@ -0,0 +1,6 @@
+## Building
+To build the smart contract, first install dependencies:
+
+``` npm install openzeppelin-solidity ```
+
+Then compile the smart contract using `solc`.
--- test.sol
+++ test.sol
@@ -1,234 +1,100 @@
-pragma solidity ^0.5.0;
+pragma solidity 0.5.0;
 
-// ----------------------------------------------------------------------------
-// Safe maths
-// ----------------------------------------------------------------------------
-library SafeMath {
-    function add(uint a, uint b) internal pure returns (uint c) {
-        c = a + b;
-        require(c >= a);
-    }
-    function sub(uint a, uint b) internal pure returns (uint c) {
-        require(b <= a);
-        c = a - b;
-    }
-    function mul(uint a, uint b) internal pure returns (uint c) {
-        c = a * b;
-        require(a == 0 || c / a == b);
-    }
-    function div(uint a, uint b) internal pure returns (uint c) {
-        require(b > 0);
-        c = a / b;
-    }
-}
+import 'openzeppelin-solidity/ownership/Ownable.sol';
+import 'openzeppelin-solidity/token/ERC20/ERC20.sol';
+import 'openzeppelin-solidity/token/ERC20/ERC20Detailed.sol';
 
-// ----------------------------------------------------------------------------
-// ERC Token Standard #20 Interface
-// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
-// ----------------------------------------------------------------------------
-contract ERC20Interface {
-    function totalSupply() public view returns (uint);
-    function balanceOf(address tokenOwner) public view returns (uint balance);
-    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
 
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-}
+/**
+ * @title ERC865Token smart contract
+ * @dev Allows delegating fees for transfers of tokens
+ */
+contract ERC865Token is ERC20, ERC20Detailed, Ownable {
+    mapping (address => uint256) private _nonces;
 
-// ----------------------------------------------------------------------------
-// Owned contract
-// ----------------------------------------------------------------------------
-contract Owned {
-    address public owner;
-    address public newOwner;
-
-    event OwnershipTransferred(address indexed _from, address indexed _to);
-
-    constructor(address _owner) public {
-        owner = _owner;
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address _newOwner) public onlyOwner {
-        newOwner = _newOwner;
-    }
-    function acceptOwnership() public {
-        require(msg.sender == newOwner);
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-        newOwner = address(0);
-    }
-}
-
-// ----------------------------------------------------------------------------
-// ERC20 Token
-// ----------------------------------------------------------------------------
-contract ERC20Token is ERC20Interface, Owned {
-    using SafeMath for uint;
-
-    string public symbol;
-    string public  name;
-    uint8 public decimals;
-    uint _totalSupply;
-
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
-
-    // ------------------------------------------------------------------------
-    // Constructor
-    // ------------------------------------------------------------------------
-    constructor(string memory _symbol, string memory _name, uint8 _decimals, uint _initialSupply, address _tokenOwner) public {
-        symbol = _symbol;
-        name = _name;
-        decimals = _decimals;
-        _totalSupply = _initialSupply;
-        balances[_tokenOwner] = _initialSupply;
-        emit Transfer(address(0), _tokenOwner, _totalSupply);
-    }
-
-    // ------------------------------------------------------------------------
-    // Total supply
-    // ------------------------------------------------------------------------
-    function totalSupply() public view returns (uint) {
-        return _totalSupply.sub(balances[address(0)]);
-    }
-
-    // ------------------------------------------------------------------------
-    // Get the token balance for account `tokenOwner`
-    // ------------------------------------------------------------------------
-    function balanceOf(address tokenOwner) public view returns (uint balance) {
-        return balances[tokenOwner];
-    }
-
-    // ------------------------------------------------------------------------
-    // Transfer the balance from token owner's account to `to` account
-    // - Owner's account must have sufficient balance to transfer
-    // - 0 value transfers are allowed
-    // ------------------------------------------------------------------------
-    function transfer(address to, uint tokens) public returns (bool success) {
-        balances[msg.sender] = balances[msg.sender].sub(tokens);
-        balances[to] = balances[to].add(tokens);
-        emit Transfer(msg.sender, to, tokens);
-        return true;
-    }
-
-    // ------------------------------------------------------------------------
-    // Token owner can approve for `spender` to transferFrom(...) `tokens`
-    // from the token owner's account
-    //
-    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-    // recommends that there are no checks for the approval double-spend attack
-    // as this should be implemented in user interfaces
-    // ------------------------------------------------------------------------
-    function approve(address spender, uint tokens) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        return true;
-    }
-
-    // ------------------------------------------------------------------------
-    // Transfer `tokens` from the `from` account to the `to` account
-    //
-    // The calling account must already have sufficient tokens approve(...)-d
-    // for spending from the `from` account and
-    // - From account must have sufficient balance to transfer
-    // - Spender must have sufficient allowance to transfer
-    // - 0 value transfers are allowed
-    // ------------------------------------------------------------------------
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        balances[from] = balances[from].sub(tokens);
-        allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
-        balances[to] = balances[to].add(tokens);
-        emit Transfer(from, to, tokens);
-        return true;
-    }
-
-    // ------------------------------------------------------------------------
-    // Returns the amount of tokens approved by the owner that can be
-    // transferred to the spender's account
-    // ------------------------------------------------------------------------
-    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
-        return allowed[tokenOwner][spender];
-    }
-
-    // ------------------------------------------------------------------------
-    // Don't accept ETH
-    // ------------------------------------------------------------------------
-    function () external payable {
-        revert();
-    }
-}
-
-contract ERC865Token is ERC20Token {
-
+    /**
+     * @dev Ownership and the initial supply of tokens are transferred to `_owner` during creation.
+     * So `msg.sender` is not necessarily the `_owner` of the new smart contract.
+     */
     constructor (address _owner, string memory _symbol, string memory _name, uint8 _decimals, uint _initialSupply)
-        Owned(_owner)
-        ERC20Token(_symbol, _name, _decimals, _initialSupply, _owner)
         public
-        {
-            
+        Ownable()
+        ERC20Detailed(_name, _symbol, _decimals) {
+            _transferOwnership(_owner);
+            _mint(_owner, _initialSupply);
+        }
+    
+    /**
+    * @dev Transfer token for a specified address
+    * Called by a third party to execute a transfer (similar to transfer from, but using a signature)
+    * @param _signature Signed hash of the transfer parameters.
+    * @param _from The address which you want to send tokens from.
+    * @dev _signature must be produced using the private key of the _from account.
+    * @param _to The address to transfer to.
+    * @param _value The amount to be transferred.
+    * @param _nonce The nonce of this transfer.
+    * @dev _nonce values must never repeat. To get the current nonce use `getAccountNonce` function.
+    */
+    function transferPreSigned(bytes memory _signature, address _from, address _to, uint256 _value, uint256 _nonce)
+        public
+        onlyOwner {
+            require(_nonces[_from] == _nonce);
+            bytes32 message = keccak256(abi.encodePacked(_from, _to, _value, _nonce));
+            require(recoverSigner(message, _signature) == _from);
+
+            _nonces[_from] += 1;
+            _transfer(_from, _to, _value);
         }
 
+    /**
+     * @return The current account nonce.
+     */
+    function getAccountNonce(address _account) 
+        public 
+        view 
+        returns (uint256) {
+            return _nonces[_account];
+        }
+
+    /**
+     * @dev Internal function to parse signature parameters from a byte array.
+     */
     function splitSignature(bytes memory _signature)
         internal
         pure
-        returns (uint8, bytes32, bytes32)
-    {
-        require(_signature.length == 65);
+        returns (uint8, bytes32, bytes32) {
+            require(_signature.length == 65);
 
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
+            bytes32 r;
+            bytes32 s;
+            uint8 v;
 
-        assembly {
-            // first 32 bytes, after the length prefix
-            r := mload(add(_signature, 32))
-            // second 32 bytes
-            s := mload(add(_signature, 64))
-            // final byte (first byte of the next 32 bytes)
-            v := byte(0, mload(add(_signature, 96)))
+            assembly {
+                // first 32 bytes, after the length prefix
+                r := mload(add(_signature, 32))
+                // second 32 bytes
+                s := mload(add(_signature, 64))
+                // final byte (first byte of the next 32 bytes)
+                v := byte(0, mload(add(_signature, 96)))
+            }
+
+            return (v, r, s);
         }
 
-        return (v, r, s);
-    }
-
+    /**
+     * @dev Internal function to recover signature address. Uses erecover to check the signature.
+     */
     function recoverSigner(bytes32 message, bytes memory _signature)
         internal
         pure
-        returns (address)
-    {
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
+        returns (address) {
+            uint8 v;
+            bytes32 r;
+            bytes32 s;
 
-        (v, r, s) = splitSignature(_signature);
+            (v, r, s) = splitSignature(_signature);
 
-        return ecrecover(message, v, r, s);
-    }
-    
-    function approveFrom(address _from, address _to, uint256 _value)
-        internal
-    {
-        allowed[_from][_to] = allowed[_from][_to].add(_value);    
-    }
-    
-    function transferPreSigned(bytes memory _signature, address _from, address _to, uint256 _value)
-        public
-        onlyOwner
-    {
-        //TODO: add nonce check
-        bytes32 message = keccak256(abi.encodePacked(_from, _to, _value));
-        require(recoverSigner(message, _signature) == _from);
-        
-        approveFrom(_from, msg.sender, _value);
-        transferFrom(_from, _to, _value);
-    }
+            return ecrecover(message, v, r, s);
+        }
 }
 
