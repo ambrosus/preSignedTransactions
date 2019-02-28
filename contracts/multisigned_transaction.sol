5c0f171872a21450afb6e0e8719b7ec5f27b66be
Gega Anatoly <gega.anatoly@apriorit.com>
2019-02-28 18:13:10 +0200

    Added comments

--- contracts/multisigned_transaction.sol
+++ contracts/multisigned_transaction.sol
@@ -3,12 +3,17 @@ pragma solidity ^0.5.2;
 import 'openzeppelin-solidity/math/SafeMath.sol';
 import 'openzeppelin-solidity/token/ERC20/ERC20.sol';
 
+
+/**
+ * @title DelegateERC20 smart contract
+ * @dev Controls execution of ERC20 token transactions
+*/
 contract DelegateERC20 {
     using SafeMath for uint256;
     
-    ERC20 private _contractAddress;
-    uint constant _neededApprovals = 3;
-    uint256 constant _callLifeTime = 300000; //seconds
+    ERC20 private _contractAddress;             //address of controlled contract
+    uint constant _neededApprovals = 3;         //amount of approves needed for transaction execution
+    uint256 constant _callLifeTime = 300000;    //transaction lifetime in seconds
     uint256 private _transactionNonce = 0;
     
     
@@ -16,32 +21,56 @@ contract DelegateERC20 {
         mapping (address => bool) approvers;
         uint amountOfApprovals;
         uint256 timestamp;
-        uint arrayIndex;
+        uint arrayIndex;    //index in array of pending transactions
         
-        bytes transaction;
-        address executor;
+        bytes transaction;  //function selector + encoded arguments
+        address executor;   //contract, that should perform call
     }
     
     bytes32[] _pendingTransactions;
     mapping (bytes32 => Transaction) private _transactions;
     mapping (address => bool) private _administrators;
-    mapping (bytes4 => string) private _functions;
+    mapping (bytes4 => string) private _functions;  // with this we can find function name and signature from selector
     
-    event TransactionCreated(bytes32 transactionId);
-    event TransactionCalled(bytes32 transactionId, bool success, bytes data);
-    event ApproveReceived(address from, bytes32 transactionId);
+    event TransactionCreated(bytes32 transactionId);                            //new transaction created
+    event TransactionCalled(bytes32 transactionId, bool success, bytes data);   //received needed amount of approvals and transaction executed
+    event ApprovalReceived(address from, bytes32 transactionId);                //received approval
     
     ////////////////////////////////////////////////////////////////////////////////////
 
-    function _hasApproved(address approver, bytes32 transactionId) private view returns (bool) {
+    /**
+     * @dev Checks is account approved transaction
+     * @param approver Address of account
+     * @param transactionId Identifier of transaction
+     * @return true if approver approved transaction with transactionId, and false if not
+     */
+    function _hasApproved(address approver, bytes32 transactionId) internal view returns (bool) {
         return _transactions[transactionId].approvers[approver];
     }
 
+    /**
+     * @dev Checks is account approved transaction
+     * @param approver Address of account
+     * @dev approver must have permission for approving
+     * @param transactionId Identifier of transaction
+     * @return true if approver approved transaction with transactionId, and false if not
+     */
     function hasApproved(address approver, bytes32 transactionId) public view returns (bool) {
         require (_administrators[approver], 'Approver address is not administrator');
         return _hasApproved(approver, transactionId);
     }
 
+    /**
+     * @dev Example of creating transaction, that should be approved by multiple users
+     * params of that function is parameters that needed for transaction after all approves is received
+     * in this example transaction of tokens should be approved, so we need address of receiver and amount of tokens
+     * for any function that should be approved by multiple users, should be created own function that
+     * will select executor, add needed function selector and encode parameters
+     *
+     * @param to Address of receiver
+     * @param value Amount of tokens to send
+     * @return identifier of new transaction that should be approved
+     */
     function createTransferTransaction(address to, uint value) public returns (bytes32) {
         //TODO: move signature to string and replace encoding with encodeWithSignature
         bytes4 selector = bytes4(keccak256("transfer(address,uint256)"));
@@ -61,6 +90,13 @@ contract DelegateERC20 {
         return transactionId;
     }
 
+    /**
+     * @dev Function for approving transaction, if this is last needed approval that executes transaction
+     * Reverts if sender hasn't permission to approve transaction, transaction isn't exists or already performed,
+     * transaction timeout or transaction already approved
+     *
+     * @param transactionId Identifier of transaction to approve
+     */
     function approveTransaction(bytes32 transactionId) public {
         require (_administrators[msg.sender], 'Approver address is not administrator');
         require(_transactions[transactionId].timestamp != 0, 'Transaction isn\'t exist or already performed');
@@ -69,7 +105,7 @@ contract DelegateERC20 {
         
         _transactions[transactionId].approvers[msg.sender] = true;
         _transactions[transactionId].amountOfApprovals = _transactions[transactionId].amountOfApprovals.add(1);
-        emit ApproveReceived(msg.sender, transactionId);
+        emit ApprovalReceived(msg.sender, transactionId);
         
         if (_transactions[transactionId].amountOfApprovals >= _neededApprovals) {
             (bool success, bytes memory data) = address(_transactions[transactionId].executor).call(_transactions[transactionId].transaction);
@@ -78,31 +114,58 @@ contract DelegateERC20 {
             emit TransactionCalled(transactionId, success, data);
         }
     }
-    
+
+    /**
+     * @dev Function for obtaining pending transactions
+     * @return array with pending transaction identifiers
+     */
     function getPendingTransaction() public view returns (bytes32[] memory) {
         return _pendingTransactions;
     }
-    
-    function _removePendingTransaction(bytes32 transactionId) private {
+
+    /**
+     * @dev Internal function for removing transaction from list of pending transactions
+     * @dev Replaces transaction with given identifier with last transaction from array of pending transactions
+     * @param transactionId Identifier of transaction to remove
+     */
+    function _removePendingTransaction(bytes32 transactionId) internal {
         uint index = _transactions[transactionId].arrayIndex;
         _pendingTransactions[index] = _pendingTransactions[_pendingTransactions.length - 1];
         _transactions[_pendingTransactions[index]].arrayIndex = index;
         delete _pendingTransactions[_pendingTransactions.length - 1];
         --_pendingTransactions.length;
     }
-    
+
+    /**
+     * @dev Function for obtaining pending transaction details
+     * @param transactionId Identifier of transaction
+     * @return address of contract, that should execute transaction and encoded function selector and parameters
+     */
     function getTransactionInfo(bytes32 transactionId) public view returns (address, bytes memory) {
         return (_transactions[transactionId].executor, _transactions[transactionId].transaction);
     }
-    
+
+    /**
+     * @dev Function for obtaining function name and parameters from it signature
+     * @param signature signature of function
+     * @return string with function name and parameters
+     */
     function getFunctionName(bytes4 signature) public view returns (string memory) {
         return _functions[signature];
     }
-    
+
+    /**
+     * @dev Function for creating transaction for adding new administrator
+     * @param newAdmin Address that will be added to administrators list
+     */
     function addAdministrator(address newAdmin) public {
         //create transaction for adding new administrator
     }
-    
+
+    /**
+     * @dev Function for creating transaction for removing new administrator
+     * @param admin Address that will be deleted from administrators list
+     */
     function deleteAdministrator(address admin) public {
         //create transaction for deleting administraor
     }
